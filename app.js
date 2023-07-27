9e6c182f32b037405d578cd3e226b5fa9470b779
Tinku <43386775+tanishbhandari@users.noreply.github.com>
2023-07-27 15:14:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-tinku:test123@cluster0.svx22.mongodb.net/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://admin-xxxxx:xxxx@cluster0.svx22.mongodb.net/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 const itemsSchema = {
   name: String
